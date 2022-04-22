573b6c7fdef8b5c656f4d9fdab2ad33ef5574b6e
Waveyyyy <waves-sec@protonmail.com>
2022-04-22 22:50:34 +0200

    Add template main file (empty for now)

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "Scanner.hpp"
+
+#include <SFML/Network.hpp>
+#include <SFML/System.hpp>
+
+int main(int argc, char **argv)
+{
+	if (argc < 3){
+		printf("Example : ./CMP202Project 127.0.0.1 8080");
+		exit(1);
+	}
+	auto address = sf::IpAddress(argv[1]);
+	uint16_t port = strtol(argv[2], nullptr, 10);
+
+	auto portScanner = new Scanner(address, port);
+
+	portScanner->execute();
+}
