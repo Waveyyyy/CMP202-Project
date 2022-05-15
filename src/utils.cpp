8c3ce478653c99369bdaf77044da8754849c3ae9
Waveyyyy <waves-sec@protonmail.com>
2022-05-15 02:19:59 +0200

    Implement quicksort algorithm for sorting ports before scanning
    new class, SortPorts, which is a subclass of Task so it can be easily
    threaded. This class contains the quicksort algorithm and its helper
    partition function.
    
    Added proper docstrings for the newly implemented functions
    
    Fixed formatting of files to align with project .clang-format

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,10 +1,47 @@
 #include "utils.hpp"
 
+SortPorts::SortPorts(std::vector<uint16_t> *ports, int leftMost, int rightMost)
+        : m_ports(ports), m_leftMost(leftMost), m_rightMost(rightMost - 1)
+{
+}
+
+void SortPorts::quickSortPortsList(std::vector<uint16_t> *ports, int leftMost,
+                                   int rightMost)
+{
+        if (leftMost < rightMost) {
+                int pivot = partitionPortsList(ports, leftMost, rightMost);
+                quickSortPortsList(ports, leftMost, pivot - 1);
+                quickSortPortsList(ports, pivot + 1, rightMost);
+        }
+}
+
+int SortPorts::partitionPortsList(std::vector<uint16_t> *ports, int leftMost,
+                                  int rightMost)
+{
+        int pivotIndex = rightMost;
+        int storeIndex = leftMost - 1;
+
+        for (int i = leftMost; i < rightMost; ++i) {
+                if (ports->at(i) <= ports->at(pivotIndex)) {
+                        ++storeIndex;
+
+                        std::iter_swap(ports->begin() + i,
+                                       ports->begin() + pivotIndex);
+                }
+                std::iter_swap(ports->begin() + pivotIndex,
+                               ports->begin() + (storeIndex + 1));
+        }
+        return storeIndex + 1;
+}
+
+void SortPorts::execute()
+{
+        quickSortPortsList(m_ports, m_leftMost, m_rightMost);
+}
 
 bool validPort(std::string &port)
 {
-        if (std::stoi(port) > 65535 || std::stoi(port) < 0)
-        {
+        if (std::stoi(port) > 65535 || std::stoi(port) < 0) {
                 return false;
         }
 
@@ -20,41 +57,40 @@ void argumentToPortslist(const char *data, std::vector<uint16_t> *ports)
         // iterate through values delimited by commas in the provided ports string
         while (getline(rawPort, tmp, ',')) {
                 // check if user has provided a range of ports
-                if (tmp.find('-') == std::string::npos)
-                {
-                        if (!validPort(tmp))
-                        {
-                                fprintf(stderr, "Invalid Port : %s", tmp.c_str());
+                if (tmp.find('-') == std::string::npos) {
+                        if (!validPort(tmp)) {
+                                fprintf(stderr, "Invalid Port : %s",
+                                        tmp.c_str());
                                 exit(1);
                         }
                         ports->push_back(std::stoi(tmp));
                         continue;
                 }
                 // allows user to supply "-" as a shorthand for 0-65535
-                if (std::strcmp(tmp.c_str(), "-") == 0)
-                {
-                        for(int i = 0; i <= 65535; ++i)
-                        {
+                if (std::strcmp(tmp.c_str(), "-") == 0) {
+                        for (int i = 0; i <= 65535; ++i) {
                                 ports->push_back(i);
                         }
                         break;
                 }
                 std::string rangeStart = tmp.substr(0, tmp.find('-'));
-                std::string rangeEnd = tmp.substr(tmp.find('-')+1, tmp.length());
+                std::string rangeEnd =
+                        tmp.substr(tmp.find('-') + 1, tmp.length());
 
-                if (std::stoi(rangeStart) > std::stoi(rangeEnd))
-                {
-                        fprintf(stderr, "Starting value cannot be greater than end value: %s-%s", rangeStart.c_str(), rangeEnd.c_str());
+                if (std::stoi(rangeStart) > std::stoi(rangeEnd)) {
+                        fprintf(stderr,
+                                "Starting value cannot be greater than end value: %s-%s",
+                                rangeStart.c_str(), rangeEnd.c_str());
                         exit(1);
                 }
 
-                if (!((validPort(rangeStart) && (validPort(rangeEnd)))))
-                {
-                        fprintf(stderr, "Range cannot exceed 65535: %s-%s", rangeStart.c_str(), rangeEnd.c_str());
+                if (!((validPort(rangeStart) && (validPort(rangeEnd))))) {
+                        fprintf(stderr, "Range cannot exceed 65535: %s-%s",
+                                rangeStart.c_str(), rangeEnd.c_str());
                         exit(1);
                 }
-                for (int i = std::stoi(rangeStart); i < std::stoi(rangeEnd); ++i)
-                {
+                for (int i = std::stoi(rangeStart); i < std::stoi(rangeEnd);
+                     ++i) {
                         ports->push_back(i);
                 }
         }
--- src/utils.hpp
+++ src/utils.hpp
@@ -1,9 +1,48 @@
 #ifndef CMP202PROJECT_UTILS_HPP
 #define CMP202PROJECT_UTILS_HPP
 
+#include "tasks.hpp"
+
 #include <vector>
 #include <cstring>
 #include <sstream>
+
+class SortPorts : public Task {
+    public:
+        /// Non-default constructor
+        ///
+        /// @ports Pointer to (unsorted) ports vector
+        /// @leftMost Optional argument specifying the index where smallest values should begin
+        /// @rightMost The size of the vector minus 1
+        ///
+        SortPorts(std::vector<uint16_t> *ports, int leftMost, int rightMost);
+
+        /// Recursive function implementing the quicksort algorithm
+        /// @ports Pointer to the ports vector
+        /// @leftMost Index at which sorted values will be placed
+        /// @rightMost Index of value that will be sorted next
+        void quickSortPortsList(std::vector<uint16_t> *ports, int leftMost,
+                                int rightMost);
+
+        /// Effectively searches through a subarray and sorts the values within
+        /// @ports Pointer to the ports vector
+        /// @leftMost Index at which subarray starts
+        /// @rightMost Index at which subarray ends
+        /// @return Pivot point used with quickSortPortsList which gives new subarray in next recursive call
+        static int partitionPortsList(std::vector<uint16_t> *ports,
+                                      int leftMost, int rightMost);
+
+        /// Main driver function for SortPorts class
+        ///
+        /// Runs the recursive function, quickSortPortsList
+        void execute() override;
+
+    private:
+        // Member variables
+        std::vector<uint16_t> *m_ports;
+        int m_leftMost;
+        int m_rightMost;
+};
 
 bool validPort(std::string &port);
 
