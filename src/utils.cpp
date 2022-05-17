b6b26267ff2cdaf594d53c2783834083096a775f
Waveyyyy <waves-sec@protonmail.com>
2022-05-17 20:14:01 +0200

    Fixed recursive quicksort implementation
    Misunderstanding of how the algorithm worked was causing issues ... only
    some of the time. :/
    
    Added comments for all functions within the utils.* files
    Updated existing docstrings and added them to functions previously
    lacking

--- src/utils.cpp
+++ src/utils.cpp
@@ -9,8 +9,15 @@ void SortPorts::quickSortPortsList(std::vector<uint16_t> *ports, int leftMost,
                                    int rightMost)
 {
         if (leftMost < rightMost) {
+                // find pivot element
+                // elements smaller than pivot are on the left
+                // elements larger than pivot are on the right
                 int pivot = partitionPortsList(ports, leftMost, rightMost);
+
+                // recursive call on the left of pivot
                 quickSortPortsList(ports, leftMost, pivot - 1);
+
+                // recursive call on the right of pivot
                 quickSortPortsList(ports, pivot + 1, rightMost);
         }
 }
@@ -18,19 +25,29 @@ void SortPorts::quickSortPortsList(std::vector<uint16_t> *ports, int leftMost,
 int SortPorts::partitionPortsList(std::vector<uint16_t> *ports, int leftMost,
                                   int rightMost)
 {
-        int pivotIndex = rightMost;
+        // rightmost value is pivot
+        int pivot = ports->at(rightMost);
+
+        // pointer for the greater element
         int storeIndex = leftMost - 1;
 
+        // loop through each element and compare against pivot
         for (int i = leftMost; i < rightMost; ++i) {
-                if (ports->at(i) <= ports->at(pivotIndex)) {
+                if (ports->at(i) <= pivot) {
+                        // if element smaller than or equal to pivot is found
+                        // swap with greater element found at storeIndex
                         ++storeIndex;
 
+                        // swapping of current value at i with value at storeIndex
                         std::iter_swap(ports->begin() + i,
-                                       ports->begin() + pivotIndex);
+                                       ports->begin() + storeIndex);
                 }
-                std::iter_swap(ports->begin() + pivotIndex,
-                               ports->begin() + (storeIndex + 1));
         }
+        // swap pivot with the greater element found at storeIndex
+        std::iter_swap(ports->begin() + rightMost,
+                       ports->begin() + (storeIndex + 1));
+
+        // return index from where the partition is done
         return storeIndex + 1;
 }
 
@@ -41,20 +58,23 @@ void SortPorts::execute()
 
 bool validPort(std::string &port)
 {
+        // check if port is within the bounds of a unsigned short (0-65535)
         if (std::stoi(port) > 65535 || std::stoi(port) < 0) {
                 return false;
         }
-
         return true;
 }
 
 void argumentToPortslist(const char *data, std::vector<uint16_t> *ports)
 {
+        // convert the raw port string from argv (*data) to a stringstream
+        // this allows for using getline to split on commas
         std::stringstream rawPort;
         rawPort << data;
         std::string tmp;
 
         // iterate through values delimited by commas in the provided ports string
+        // values between commas are put into the tmp variable
         while (getline(rawPort, tmp, ',')) {
                 // check if user has provided a range of ports
                 if (tmp.find('-') == std::string::npos) {
@@ -66,17 +86,28 @@ void argumentToPortslist(const char *data, std::vector<uint16_t> *ports)
                         ports->push_back(std::stoi(tmp));
                         continue;
                 }
-                // allows user to supply "-" as a shorthand for 0-65535
+                // allows user to supply "-" as a shorthand for scanning all ports
                 if (std::strcmp(tmp.c_str(), "-") == 0) {
                         for (int i = 0; i <= 65535; ++i) {
                                 ports->push_back(i);
                         }
                         break;
                 }
+
+                // initialise two variables one containing the value on the left side of the "-" symbol
+                // and one containing the value on the right side of the "-" symbol
                 std::string rangeStart = tmp.substr(0, tmp.find('-'));
                 std::string rangeEnd =
                         tmp.substr(tmp.find('-') + 1, tmp.length());
 
+                // if either value is not deemed a valid port then error out
+                if (!((validPort(rangeStart) && (validPort(rangeEnd))))) {
+                        fprintf(stderr, "Range cannot exceed 65535: %s-%s",
+                                rangeStart.c_str(), rangeEnd.c_str());
+                        exit(1);
+                }
+
+                // if the starting value is greater than the end value error out
                 if (std::stoi(rangeStart) > std::stoi(rangeEnd)) {
                         fprintf(stderr,
                                 "Starting value cannot be greater than end value: %s-%s",
@@ -84,11 +115,7 @@ void argumentToPortslist(const char *data, std::vector<uint16_t> *ports)
                         exit(1);
                 }
 
-                if (!((validPort(rangeStart) && (validPort(rangeEnd))))) {
-                        fprintf(stderr, "Range cannot exceed 65535: %s-%s",
-                                rangeStart.c_str(), rangeEnd.c_str());
-                        exit(1);
-                }
+                // loop through range and add i to ports vector
                 for (int i = std::stoi(rangeStart); i < std::stoi(rangeEnd);
                      ++i) {
                         ports->push_back(i);
--- src/utils.hpp
+++ src/utils.hpp
@@ -11,24 +11,25 @@ class SortPorts : public Task {
     public:
         /// Non-default constructor
         ///
-        /// @ports Pointer to (unsorted) ports vector
-        /// @leftMost Optional argument specifying the index where smallest values should begin
-        /// @rightMost The size of the vector minus 1
+        /// \param ports Pointer to (unsorted) ports vector
+        /// \param leftMost Argument specifying the index where smallest values should begin
+        /// \param rightMost The size of the vector minus 1
         ///
         SortPorts(std::vector<uint16_t> *ports, int leftMost, int rightMost);
 
         /// Recursive function implementing the quicksort algorithm
-        /// @ports Pointer to the ports vector
-        /// @leftMost Index at which sorted values will be placed
-        /// @rightMost Index of value that will be sorted next
+        /// \param ports Pointer to the ports vector
+        /// \param leftMost Index at which sorted values will be placed
+        /// \param rightMost Index of value that will be sorted next
         void quickSortPortsList(std::vector<uint16_t> *ports, int leftMost,
                                 int rightMost);
 
         /// Effectively searches through a subarray and sorts the values within
-        /// @ports Pointer to the ports vector
-        /// @leftMost Index at which subarray starts
-        /// @rightMost Index at which subarray ends
-        /// @return Pivot point used with quickSortPortsList which gives new subarray in next recursive call
+        ///
+        /// \param ports Pointer to the ports vector
+        /// \param leftMost Index at which subarray starts
+        /// \param rightMost Index at which subarray ends
+        /// \return Pivot point used with quickSortPortsList which gives new subarray in next recursive call
         static int partitionPortsList(std::vector<uint16_t> *ports,
                                       int leftMost, int rightMost);
 
@@ -44,8 +45,16 @@ class SortPorts : public Task {
         int m_rightMost;
 };
 
+/// Checks if a supplied port value is within the bounds of a unsigned short
+///
+/// \param port The string representation of the port to be checked
+/// \return true or false depending if port is between 0 and 65535
 bool validPort(std::string &port);
 
+/// Converts the supplied argument string of ports to a vector of ports
+///
+/// \param data The string of ports supplied in argv[2]
+/// \param ports The vector to store the individual ports into
 void argumentToPortslist(const char *data, std::vector<uint16_t> *ports);
 
 #endif //CMP202PROJECT_UTILS_HPP
